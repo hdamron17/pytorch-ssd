3edcaae1eaa752c1e273c43006929d2c7753eee2
Hunter Damron <hdamron@email.sc.edu>
2020-06-16 18:06:03 -0400

    Don't fail on missing file

--- prune_mtsd_dataset.py
+++ prune_mtsd_dataset.py
@@ -2,6 +2,7 @@ import vision.datasets.mtsd_default_classes as dflt
 from sys import argv
 import pathlib
 import json
+import os
 
 if len(argv) != 3:
     print("Usage: prune_mtsd_dataset.py <dataset> <orig split>")
@@ -13,6 +14,8 @@ orig_split = pathlib.Path(argv[2])
 ids = map(str.strip, open(orig_split, 'r').readlines())
 for id in ids:
     anno_file = dataset_path / "annotations" / f"{id}.json"
+    if not os.path.isfile(anno_file):
+        continue
     anno = json.load(open(anno_file, 'r'))
     labels = (obj["label"] for obj in anno["objects"])
     classes = list(filter(None, map(dflt.convert_label, labels)))
