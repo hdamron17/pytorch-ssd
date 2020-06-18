37ed65098cdde97ede8e353e472e4fdddabaf6d6
Hunter Damron <hdamron@email.sc.edu>
2020-06-18 10:48:40 -0400

    Changed to take all warning signs instead of just pedestrian

--- vision/datasets/mtsd_default_classes.py
+++ vision/datasets/mtsd_default_classes.py
@@ -1,9 +1,10 @@
 ## Taken from https://github.com/jacky841102/HD_map_updating/blob/master/sign_detector/preprocess_script/trafficsigns_to_detectron_6_class.py
 
-#                 0      1            2         3: black and white    4:red circle               5
-CATEGORIES =  ['stop', 'yield', 'do_not_enter', 'other_regulatory', 'other_prohibitory', 'warning_pedestrians']
+#                 0      1            2         3: black and white    4:red circle           5
+CATEGORIES =  ['stop', 'yield', 'do_not_enter', 'other_regulatory', 'other_prohibitory', 'warning']
 
 def convert_label(label):
+    class_id = -1  # empty value
     if 'regulatory--stop--g1' in label:
         class_id = 0
     elif 'regulatory--yield--g1' in label:
@@ -41,11 +42,14 @@ def convert_label(label):
         class_id = 4
     elif 'regulatory--no-left-turn--g1' in label:
         class_id = 4
-    elif 'warning--pedestrians-crossing--g4' in label:
-        class_id = 5
+    elif label.startswith('warning'):
+        if 'warning--railroad-crossing--g4' in label:
+            class_id = -1
+        else:
+            class_id = 5
     elif 'regulatory--bicycles-only--g3' in label:
         class_id = -1
     else:
         class_id = -1
 
-    return class_id if class_id >= 0 else None
+    return class_id + 1 if class_id >= 0 else None
